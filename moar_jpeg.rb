fe885f2ca4b28707c7941a01a03861852ba130d6
Alexis Viguié <alexis.viguie@epitech.eu>
2016-11-22 18:56:00 +0100

    completed code

--- moar_jpeg.rb
+++ moar_jpeg.rb
@@ -16,7 +16,7 @@ Telegram::Bot::Client.run(ARGV[0]) do |moar_jpeg|
   moar_jpeg.listen do |message|
     bot.api.send_message(chat_id: message.chat.id, text: "Hi, #{message.from.first_name}! Please send me your images as photos. Every other messages (text, files, stickers... will be ignored. Have fun!") if message.text == "/start"
     if message.photo[0]
-      compression = if message.caption.to_i.to_s == message_caption && message.caption.to_i.between?(1,100)
+      compression = if message.caption && (message.caption.to_i.to_s == message_caption && message.caption.to_i.between?(1,100))
                       message.caption.to_i
                     else
                       90
